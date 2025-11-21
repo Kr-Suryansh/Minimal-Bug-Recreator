b952cfd6db9732a56bb151ff17ec57d1a704d625
lalitsureliya-2005 <lalitsureliya8@gmail.com>
2025-11-21 22:41:33 +0530

    created generateroute.js

--- Backend/src/routes/generate.route.js
+++ Backend/src/routes/generate.route.js
@@ -1,1 +1,8 @@
 
+const express = require('express');
+const router = express.Router();
+const { generateMRE } = require('../controllers/generate.controller');
+
+router.post('/', generateMRE);
+
+module.exports = router;
